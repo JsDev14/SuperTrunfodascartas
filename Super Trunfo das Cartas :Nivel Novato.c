4d617597ca233b7cbb015a7b9d1d52d30676b4c4
JsDev14 <135184931+JsDev14@users.noreply.github.com>
2025-05-14 01:52:54 +0000

    Alteração 01.c

---
+++ Super Trunfo das Cartas :Nivel Novato.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+    char estado[10]; 
+    char estado2[10];
+    char codigocarta[50];
+    char codigocarta2[50];
+    char nomeCidade[50];
+    char nomeCidade2[50];
+    int populacao, populacao2;
+    float areakm, pib;
+    int pontosturisticos;
+
+    printf("Carta 1:\n");
+
+    printf("Estado: \n");
+    scanf("%s",&estado);
+
+    printf("Código:\n");
+    scanf("%s",&codigocarta);
+    
+    printf("Nome da Cidade:\n");
+    scanf("%s",&nomeCidade);
+
+    printf("População:\n");
+    scanf("%d",&populacao);
+
+    printf("Área em Km: \n");
+    scanf("%f",&areakm);
+
+    printf("Pib:\n");
+    scanf("%f", &pib);
+
+    printf("Número de Pontos Turísticos: \n");
+    scanf("%d", &pontosturisticos);
+
+    
+
+
+
+
+
+    
+}
